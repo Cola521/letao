3c0af4c70cec7492e488854c35a4c7861a576145
zhaojinxin <zjx@example.com>
2018-04-08 10:50:08 +0800

    测试

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,39 +1,40 @@
-$(function(){
+$(function() {
     var page = 1;
     var pageSize = 5;
-    function render(){
+
+    function render() {
         $.ajax({
-            type:"get",
-            url:"/user/queryUser",
-            dataType:"json",
-            data:{
-                page:page,
-                pageSize:pageSize
+            type: "get",
+            url: "/user/queryUser",
+            dataType: "json",
+            data: {
+                page: page,
+                pageSize: pageSize
             },
-            success:function(info){
+            success: function(info) {
                 console.log(info);
-                $('.table thead').html(template('td',info));
+                $('.table thead').html(template('td', info));
             }
         });
     }
     render();
     // 禁用启用
-    $('.table').on('click','thead td',function(){
+    $('.table').on('click', 'thead td', function() {
         var id = $(this).data('id');
         var isDelete = $(this).data('delete');
-        console.log($(this).data('id'),$(this).data('delete'));
-         $.ajax({
-            type:'post',
-            url:'/user/updateUser',
-            dataType:"json",
-            data:{
-                id:id,
-                isDelete:isDelete
+        console.log($(this).data('id'), $(this).data('delete'));
+        $.ajax({
+            type: 'post',
+            url: '/user/updateUser',
+            dataType: "json",
+            data: {
+                id: id,
+                isDelete: isDelete
             },
-            success:function(info){
-                 console.log('111');
-            //$('').html(template('',info));
-            render();
+            success: function(info) {
+                console.log('111');
+                //$('').html(template('',info));
+                render();
             }
         });
     });
